77d1dc97a3755699a997154ec4ec45ffef6f24a3
Yun Shen Tan <rrongan@gmail.com>
2017-10-18 19:33:08 +0100

    Added password hashing function

--- models/student.js
+++ models/student.js
@@ -1,4 +1,12 @@
-var mongoose = require('mongoose');
+/*
+*  Mongoose password hashing - https://stackoverflow.com/questions/14588032/mongoose-password-hashing
+*                            - https://www.mongodb.com/blog/post/password-authentication-with-mongoose-part-1
+*
+* */
+
+var mongoose = require('mongoose'),
+    bcrypt = require('bcrypt'),
+    SALT_WORK_FACTOR = 10;
 
 var StudentSchema = new mongoose.Schema({
     name: String,
@@ -15,5 +23,33 @@ var StudentSchema = new mongoose.Schema({
         }
     }
 });
+
+StudentSchema.pre('save', function(next) {
+    var user = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err);
+
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+StudentSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
 
 module.exports = mongoose.model('Student', StudentSchema);
