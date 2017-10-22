71cfd65b3ce470e12cc3a46ddd7004372511d6fe
Yun Shen Tan <rrongan@gmail.com>
2017-10-22 19:25:40 +0100

    Refactored session and login function

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,11 @@
 var express = require('express');
+var auth = require('../routes/authentication')
 var router = express.Router();
 
 var sess;
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    sess = req.session;
-    if(sess.username) {
-        res.redirect('/main');
-    }
-    else {
-        res.render('index.ejs',{ title: 'University Review Website' });
-    }
+router.get('/',auth.sessionChecker, function(req, res, next) {
+    res.render('index.ejs',{ title: 'University Review Website' });
 });
 
 module.exports = router;
