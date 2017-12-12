e8817392001d4408ce0f0baae5734f5ac4ea9995
Yun Shen Tan <rrongan@gmail.com>
2017-12-12 17:04:33 +0000

    Changed port in app

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-var port = process.env.PORT || CONFIG.port;
+var port = process.env.PORT || 3000;
 app.listen(port);
 
 // uncomment after placing your favicon in /public
