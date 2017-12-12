f17865217122de1be2acb1d3338dff3857bd8480
Yun Shen Tan <rrongan@gmail.com>
2017-12-12 16:58:13 +0000

    Add port listen to app

--- app.js
+++ app.js
@@ -27,6 +27,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+var port = process.env.PORT || CONFIG.port;
+app.listen(port);
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
